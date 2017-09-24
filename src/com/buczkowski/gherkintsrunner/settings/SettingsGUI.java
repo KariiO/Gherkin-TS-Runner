f27ce2dc0cde130f0d7777e88727869795284d6e
KariiO <buczkowski.korneliusz@gmail.com>
2017-09-24 15:54:05 +0200

    Change button with FileChooser to TextFieldWithBrowseButton

--- src/com/buczkowski/gherkintsrunner/settings/SettingsGUI.java
+++ src/com/buczkowski/gherkintsrunner/settings/SettingsGUI.java
@@ -1,25 +1,19 @@
 package com.buczkowski.gherkintsrunner.settings;
 
-import com.intellij.openapi.fileChooser.FileChooser;
-import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 
 import javax.swing.*;
-
-import static com.buczkowski.gherkintsrunner.settings.Config.DEFAULT_FEATURE_REGEX;
-import static com.buczkowski.gherkintsrunner.settings.Config.DEFAULT_SCENARIO_REGEX;
 
 public class SettingsGUI {
     private JTextField featureRegexTextField;
     private JTextField scenarioRegexTextField;
     private JPanel rootPanel;
-    private JTextField protractorCmdTextField;
-    private JTextField protractorConfigJsTextField;
-    private JButton cmdPathButton;
-    private JButton configPathButton;
-    private JTextField featuresDirPathTextField;
-    private JButton configFeaturesDirButton;
+    private TextFieldWithBrowseButton protractorCmdTextField;
+    private TextFieldWithBrowseButton protractorConfigJsTextField;
+    private TextFieldWithBrowseButton featuresDirPathTextField;
+
     private Config mConfig;
 
     JPanel getRootPanel() {
@@ -28,22 +22,30 @@ public class SettingsGUI {
 
     void createUI(Project project) {
         mConfig = Config.getInstance(project);
+
+        if(mConfig == null)
+            return;
+
         featureRegexTextField.setText(mConfig.getFeatureRegex());
         scenarioRegexTextField.setText(mConfig.getScenarioRegex());
         protractorCmdTextField.setText(mConfig.getProtractorCmdPath());
         protractorConfigJsTextField.setText(mConfig.getProtractorConfigJsPath());
-        featuresDirPathTextField.setText(mConfig.getFeaturesDir());
+        featuresDirPathTextField.setText(mConfig.getFeaturesDirPath());
 
-        cmdPathButton.addActionListener(e -> protractorCmdTextField.setText(openFileChooser(project, true)));
-        configPathButton.addActionListener(e -> protractorConfigJsTextField.setText(openFileChooser(project, true)));
-        configFeaturesDirButton.addActionListener(e -> featuresDirPathTextField.setText(openFileChooser(project, false)));
-    }
+        protractorCmdTextField.addBrowseFolderListener(
+                "Select Your Protractor Cmd File",
+                "Specifies the protractor cmd file that command will be executed on", null,
+                FileChooserDescriptorFactory.createSingleFileDescriptor("cmd"));
 
-    private String openFileChooser(Project project, boolean isFile) {
-        FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(isFile, !isFile, false, false, false, false);
-        VirtualFile file = FileChooser.chooseFile(fileChooserDescriptor, getRootPanel(),project, null);
+        protractorConfigJsTextField.addBrowseFolderListener(
+                "Select Your Protractor Config File",
+                "Specifies the protractor config file that will be passed to execution command", null,
+                FileChooserDescriptorFactory.createSingleFileDescriptor("js"));
 
-        return file == null ? "" : file.getPath();
+        featuresDirPathTextField.addBrowseFolderListener(
+                "Select Your Features Directory",
+                "Specifies the directory that contains all files with .feature extension", null,
+                FileChooserDescriptorFactory.createSingleFolderDescriptor());
     }
 
     boolean isModified() {
@@ -52,22 +54,16 @@ public class SettingsGUI {
         modified |= !scenarioRegexTextField.getText().equals(mConfig.getScenarioRegex());
         modified |= !protractorCmdTextField.getText().equals(mConfig.getProtractorCmdPath());
         modified |= !protractorConfigJsTextField.getText().equals(mConfig.getProtractorConfigJsPath());
-        modified |= !featuresDirPathTextField.getText().equals(mConfig.getFeaturesDir());
+        modified |= !featuresDirPathTextField.getText().equals(mConfig.getFeaturesDirPath());
         return modified;
     }
 
     void apply() {
-        if(featureRegexTextField.getText().isEmpty())
-            featureRegexTextField.setText(DEFAULT_FEATURE_REGEX);
-
-        if(scenarioRegexTextField.getText().isEmpty())
-            scenarioRegexTextField.setText(DEFAULT_SCENARIO_REGEX);
-
         mConfig.setFeatureRegex(featureRegexTextField.getText());
         mConfig.setScenarioRegex(scenarioRegexTextField.getText());
         mConfig.setProtractorCmdPath(protractorCmdTextField.getText());
         mConfig.setProtractorConfigJsPath(protractorConfigJsTextField.getText());
-        mConfig.setFeaturesDir(featuresDirPathTextField.getText());
+        mConfig.setFeaturesDirPath(featuresDirPathTextField.getText());
     }
 
     void reset() {
@@ -75,6 +71,6 @@ public class SettingsGUI {
         scenarioRegexTextField.setText(mConfig.getScenarioRegex());
         protractorCmdTextField.setText(mConfig.getProtractorCmdPath());
         protractorConfigJsTextField.setText(mConfig.getProtractorConfigJsPath());
-        featuresDirPathTextField.setText(mConfig.getFeaturesDir());
+        featuresDirPathTextField.setText(mConfig.getFeaturesDirPath());
     }
 }

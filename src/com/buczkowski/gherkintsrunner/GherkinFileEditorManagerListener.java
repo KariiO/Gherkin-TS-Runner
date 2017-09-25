69a04c57db99ca05dfc1210ff78a29f05300dc94
KariiO <buczkowski.korneliusz@gmail.com>
2017-09-25 17:28:13 +0200

    Fix: Build relative path from features directory to .feature file

--- src/com/buczkowski/gherkintsrunner/GherkinFileEditorManagerListener.java
+++ src/com/buczkowski/gherkintsrunner/GherkinFileEditorManagerListener.java
@@ -10,18 +10,20 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class GherkinFileEditorManagerListener implements FileEditorManagerListener {
+    private Config config;
     public static String SCENARIO_REGEX;
     public static String FEATURE_REGEX;
     List<VirtualFile> gherkinFiles = new ArrayList<>();
     List<VirtualFile> openedGherkinFiles = new ArrayList<>();
 
     public GherkinFileEditorManagerListener(Project project) {
-        Config config = Config.getInstance(project);
+        config = Config.getInstance(project);
 
         if(config == null) {
             return;
@@ -37,6 +39,7 @@ public class GherkinFileEditorManagerListener implements FileEditorManagerListen
         VirtualFile newFile = event.getNewFile();
         String oldFileName = "No_old_file";
         String newFileName = "No_new_file";
+
 
         if (oldFile != null) {
             oldFileName = oldFile.getName();
@@ -75,9 +78,13 @@ public class GherkinFileEditorManagerListener implements FileEditorManagerListen
         Document rootDocument = rootEditor.getDocument();
 
         if (openedGherkinFiles.stream().noneMatch(f -> f.equals(newFile))) {
+            String path = newFile.getPath();
+            String base = config.getFeaturesDirPath();
+            String relative = new File(base).toURI().relativize(new File(path).toURI()).getPath();
             // System.out.println("[First time open] File: " + newFileName);
             openedGherkinFiles.add(newFile);
-            GherkinIconUtils gherkinIconUtils = new GherkinIconUtils(newFileName);
+
+            GherkinIconUtils gherkinIconUtils = new GherkinIconUtils(relative);
             gherkinIconUtils.generateGherkinRunIcons(rootDocument, rootEditor);
         }
 
